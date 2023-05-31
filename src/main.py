617a9124b762fafbcd7ebdd8d04d9acecfa36916
Lukas Fittl <lukas@fittl.com>
2023-05-31 14:50:47 -0400

    Allow passing "-" to data/settings flags to read from stdin
    If "-" is specified for both "-s" and "-d", the input stream is expected
    to be split by a single newline that separates the data and the
    settings JSON values.

--- src/main.py
+++ src/main.py
@@ -4,18 +4,36 @@
 import cli
 import reader
 import utils
+import sys
 
 
 if __name__ == "__main__":
     cli_args = cli.get_cli_args()
 
-    with open(cli_args["Data JSON"], "r", encoding="utf-8") as f:
-        data_json = f.read()
+    use_stdin_for_data = False
+    use_stdin_for_settings = False
+
+    data_json = None
+    if cli_args["Data JSON"] == "-":
+        use_stdin_for_data = True
+    else:
+        with open(cli_args["Data JSON"], "r", encoding="utf-8") as f:
+            data_json = f.read()
 
     settings_json = None
     if cli_args["Settings JSON"] is not None:
-        with open(cli_args["Settings JSON"], "r", encoding="utf-8") as f:
-            settings_json = f.read()
+        if cli_args["Settings JSON"] == "-":
+            use_stdin_for_settings = True
+        else:
+            with open(cli_args["Settings JSON"], "r", encoding="utf-8") as f:
+                settings_json = f.read()
+
+    if use_stdin_for_data and use_stdin_for_settings:
+        data_json, settings_json = sys.stdin.read().split("\n", 2)
+    elif use_stdin_for_data:
+        data_json = sys.stdin.read()
+    elif use_stdin_for_settings:
+        settings_json = sys.stdin.read()
 
     time_limit = cli_args["Time Limit"]
 
